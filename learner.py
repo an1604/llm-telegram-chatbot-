7ee4b2cd540cb79fdb4428fba2485a090868317f
an1604 <nataf12386@cs.colman.ac.il>
2024-08-18 16:07:22 +0300

    Update

--- learner.py
+++ learner.py
@@ -1,3 +1,9 @@
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.preprocessing import LabelEncoder
+
+
 class learner(object):
     def __init__(self):
         self.data = []
