578de2698db1fcf95904a229e44ef20b8f98e56f
an1604 <nataf12386@cs.colman.ac.il>
2024-08-18 19:36:42 +0300

    Update llm.py

--- embeddings.py
+++ embeddings.py
@@ -26,7 +26,7 @@ class embeddings(object):
         self.faq = None
 
         self.stop = False
-        self.active_learner_threshold = 1.0001  # Decide which threshold is valid to apply active learning.
+        self.active_learner_threshold = 1.19999  # Decide which threshold is valid to apply active learning.
 
     def get_nearest_neighbors(self, vector, k=3):
         index = faiss.read_index(
