bcbe845895c2f797c8b651d477ed69028436ba55
an1604 <nataf12386@cs.colman.ac.il>
2024-08-19 11:26:33 +0300

    Update learner.py Added update_mail.txt file

--- learner.py
+++ learner.py
@@ -1,3 +1,4 @@
+import os
 from threading import Thread
 from queue import Queue, Empty
 from chat_tools.send_email import send_email
@@ -8,6 +9,7 @@ class Learner(object):
         self.samples = Queue()
         self.samples_set = set()
         self.stop = False
+        self.wait_with_update = 0
         self.samples_filename = 'samples.txt'
 
         self.admins_mail = ['nataf12386@gmail.com', 'odedwar@gmail.com']
@@ -25,6 +27,7 @@ class Learner(object):
                 question, answer, knowledgebase_file_path = self.samples.get(timeout=5)
                 self.samples_set.add((question, answer))
                 self.write_sample(question, answer, knowledgebase_file_path)
+                self.wait_with_update += 1
 
                 self.update_admin()
                 # print(f'Question: {question} --> Answer: {answer}')
@@ -44,13 +47,19 @@ class Learner(object):
             print("New line wrote to the samples file")
 
     def update_admin(self):
-        for mail in self.admins_mail:
-            send_email(email_receiver=mail, display_name="DeceptifyBot", from_email="DeceptifyBot@donotreply.com",
-                       email_subject="Updates from learner",
-                       email_body=f"You have new updates from learner:\n\n {self.get_learning_detail()}")
-            print(f"mail send to {mail}")
+        if self.wait_with_update % 3 == 0:
+            script_dir = os.path.dirname(os.path.abspath(__file__))
+            update_file_path = os.path.join(script_dir, 'prompts', 'update_mail.txt')
+            with open(update_file_path, 'r') as update_file:
+                update_mail = update_file.read().format(update=self.get_learning_updates_from_file())
+                for mail in self.admins_mail:
+                    send_email(email_receiver=mail, display_name="DeceptifyBot",
+                               from_email="DeceptifyBot@donotreply.com",
+                               email_subject="Updates from learner",
+                               email_body=update_mail)
+                    print(f"mail send to {mail}")
 
-    def get_learning_detail(self):
+    def get_learning_updates_from_file(self):
         with open(self.samples_filename, 'r') as infile:
             return infile.read()
 
